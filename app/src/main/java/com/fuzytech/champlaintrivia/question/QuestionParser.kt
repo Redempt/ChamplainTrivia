09571f63fb130bddd323d0c365bb7c7f732bbc0d
Redempt <instantiable@gmail.com>
2022-09-23 14:04:47 -0400

    Parse open response questions

--- app/src/main/java/com/fuzytech/champlaintrivia/question/QuestionParser.kt
+++ app/src/main/java/com/fuzytech/champlaintrivia/question/QuestionParser.kt
@@ -5,7 +5,7 @@ import org.json.JSONObject
 
 object QuestionParser {
 
-    fun parse(input: String): List<Question<Any>> {
+    fun parse(input: String): List<Question<*>> {
         val list = JSONArray(input)
         val out = mutableListOf<Question<*>>()
         for (i in 0..list.length()) {
@@ -14,9 +14,11 @@ object QuestionParser {
             val question = entry["question"] as String
             out.add(when (type) {
                 "string" -> MultipleChoiceQuestion(question, entry["answers"] as List<String>, entry["answer"] as Int)
+                "openresponse" -> OpenResponseQuestion(question, entry["answer"] as String)
                 else -> throw IllegalArgumentException("Invalid question type $type")
             })
         }
+        return out
     }
 
 }
