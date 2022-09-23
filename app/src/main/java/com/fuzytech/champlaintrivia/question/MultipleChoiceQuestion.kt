992bd0863b59888d5c48ab8dfb0c060d14a2f067
Redempt <instantiable@gmail.com>
2022-09-23 13:59:12 -0400

    Stupid type errors

--- app/src/main/java/com/fuzytech/champlaintrivia/question/MultipleChoiceQuestion.kt
+++ app/src/main/java/com/fuzytech/champlaintrivia/question/MultipleChoiceQuestion.kt
@@ -1,6 +1,5 @@
 package com.fuzytech.champlaintrivia.question
 
-class MultipleChoiceQuestion<T>(override val question: String, val answers: List<T>, val answer: Int) :
-    Question<Int> {
+class MultipleChoiceQuestion<T>(override val question: String, val answers: List<T>, val answer: Int) : Question<Int> {
     override fun validate(answer: Int) = answer == this.answer
 }
---
+++ app/src/main/java/com/fuzytech/champlaintrivia/question/QuestionParser.kt
@@ -0,0 +1,22 @@
+package com.fuzytech.champlaintrivia.question
+
+import org.json.JSONArray
+import org.json.JSONObject
+
+object QuestionParser {
+
+    fun parse(input: String): List<Question<Any>> {
+        val list = JSONArray(input)
+        val out = mutableListOf<Question<Any>>()
+        for (i in 0..list.length()) {
+            val entry = list[i] as JSONObject
+            val type = entry["type"] as String
+            val question = entry["question"] as String
+            out.add(when (type) {
+                "string" -> MultipleChoiceQuestion(question, entry["answers"] as List<String>, entry["answer"] as Int) as Question<Any>
+                else -> throw IllegalArgumentException("Invalid question type $type")
+            })
+        }
+    }
+
+}
