cbc597fde3fdd3e390a05c36a3abbe0e2e5429dd
Redempt <instantiable@gmail.com>
2022-09-23 14:02:34 -0400

    Fix stupid type errors

--- app/src/main/java/com/fuzytech/champlaintrivia/question/QuestionParser.kt
+++ app/src/main/java/com/fuzytech/champlaintrivia/question/QuestionParser.kt
@@ -7,13 +7,13 @@ object QuestionParser {
 
     fun parse(input: String): List<Question<Any>> {
         val list = JSONArray(input)
-        val out = mutableListOf<Question<Any>>()
+        val out = mutableListOf<Question<*>>()
         for (i in 0..list.length()) {
             val entry = list[i] as JSONObject
             val type = entry["type"] as String
             val question = entry["question"] as String
             out.add(when (type) {
-                "string" -> MultipleChoiceQuestion(question, entry["answers"] as List<String>, entry["answer"] as Int) as Question<Any>
+                "string" -> MultipleChoiceQuestion(question, entry["answers"] as List<String>, entry["answer"] as Int)
                 else -> throw IllegalArgumentException("Invalid question type $type")
             })
         }
